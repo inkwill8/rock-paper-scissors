617d1c71fdd8c7ee5ea63fcb4627cb552d9e990d
Sebastien Lacasse <sebastien.lacasse101@gmail.com>
2025-01-13 00:35:59 -0500

    Add logic for playRound function
    The logic for when user inputs 'rock' is complete,
    now time to move onto paper and scissors!

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let humanScore = 0;
 let computerScore = 0;
 
 
-// write a function to randomly return one of the following strings: 'rock', 'paper', or 'scissor'
+// write a function to randomly return one of the following strings: 'rock', 'paper', or 'scissors'
 
 function getComputerChoice() {
     // store a variable with the computer's random choice
@@ -15,9 +15,10 @@ function getComputerChoice() {
     } else if (computerChoice >= 0.34 && computerChoice <= 0.67) {
         return ('Paper');
     } else {
-        return ('Scissor');
+        return ('Scissors');
     }
 }
+
 // write a function to prompt, save, and return a user input
 
 function getHumanChoice() {
@@ -34,3 +35,21 @@ function getHumanChoice() {
     }
 }
 
+function playRound(humanChoice, computerChoice) {
+   let userInput = humanChoice.toLowerCase();
+   let compInput = computerChoice.toLowerCase();
+
+    if (userInput === compInput) {
+        console.log(`It's a tie!`);
+    } else if (userInput === 'rock' && compInput === 'paper') {
+        console.log('You lose! Paper beats rock!');
+    } else if (userInput === 'rock' && compInput === 'scissors') {
+        console.log('You win! Rock beats scissors!')
+    }
+   
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
