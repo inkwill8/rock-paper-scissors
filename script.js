71f419f2330e7f4f64dabbf21e82164355d051f7
Sebastien Lacasse <sebastien.lacasse101@gmail.com>
2025-01-12 22:55:36 -0500

    Complete computer choice function
    The getComputerChoice() function is finished
     and now logs the resulting string. Now on to
     getHumanChoice().

--- script.js
+++ script.js
@@ -3,7 +3,14 @@
 function getComputerChoice() {
     // store a variable with the computer's random choice
     let computerChoice = Math.random();
-    
+    // write a condition to explain to the computer to select 1 of the 3 choices based on the random result
+    if (computerChoice <= 0.33 ) {
+        console.log('Rock');
+    } else if (computerChoice >= 0.34 && computerChoice <= 0.66) {
+        console.log('Paper');
+    } else {
+        console.log('Scissor');
+    }
 }
 
-// 
+getComputerChoice();
