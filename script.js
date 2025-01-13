4b438c56e41aebb340ef99d004ae3bd087f8091b
Sebastien Lacasse <sebastien.lacasse101@gmail.com>
2025-01-12 22:09:26 -0500

    Add computer choice random variable

---
+++ script.js
@@ -0,0 +1,9 @@
+// write a function to randomly return one of the following strings: 'rock', 'paper', or 'scissor'
+
+function getComputerChoice() {
+    // store a variable with the computer's random choice
+    let computerChoice = Math.random();
+    
+}
+
+// 
